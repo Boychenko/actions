ba2e522bf0f654a0d28b83feccaa43c56420150e
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-07-16 21:44:14 +0700

    initial

---
+++ pr/src/main.ts
@@ -0,0 +1,17 @@
+import {setFailed, getInput, warning, setCommandEcho} from '@actions/core'
+import {getOctokit, context} from '@actions/github'
+import {env} from 'process';
+
+async function run(): Promise<void> {
+  try {
+    const accessToken = getInput('access-token');
+    const octokit = getOctokit(accessToken);
+    console.log(env['WORKING_DIRECTORY']);
+    console.log(env['GITHUB_WORKSPACE']);
+    warning(env['HOME'] || '');
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+run()
